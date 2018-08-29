fa59b61084ce0ab6485fd39b374c0345c8904344
Will J <willjuszczyk@gmail.com>
2018-08-28 20:58:53 -0500

    quick upload. nothing really works

---
+++ Controllers/RedditCrawlerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetRedditApi.Controllers
+{
+    [Route("[controller]")]
+    public class RedditCrawlerController : Controller
+    {
+        public RedditCrawlerController()
+        {
+        }
+
+        [HttpGet("all")]
+        public async Task<HttpContent> All()
+        {
+            using (var client = new HttpClient())
+            {
+                var result = await client.GetAsync("https://www.reddit.com/.json").ConfigureAwait(false);
+                return result.Content;
+            }
+        }
+    }
+}
